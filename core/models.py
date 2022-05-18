4de1384c7f28b44a7ade74117b8cd11bbed37b2d
Giorgio Alefe <giorgioa2002@gmail.com>
2022-05-18 16:12:27 -0300

    Create Model and make the first migrate

--- core/models.py
+++ core/models.py
@@ -1,6 +1,12 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Meta(models.Model):
+    created = models.DateField('Created', auto_now_add=True)
+    modified = models.DateField('Modified', auto_now=True)
+    active = models.BooleanField('Active?', default=True)
+    
 
 #About Me
 
