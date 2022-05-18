5c0d80faeb26585c14342dd4ad8c060c45323a32
Giorgio Alefe <giorgioa2002@gmail.com>
2022-05-18 16:27:42 -0300

    Create AboutMeModel and rename model Meta to Base

--- core/models.py
+++ core/models.py
@@ -2,14 +2,15 @@ from pyexpat import model
 from django.db import models
 
 # Create your models here.
-class Meta(models.Model):
+class Base(models.Model):
     created = models.DateField('Created', auto_now_add=True)
     modified = models.DateField('Modified', auto_now=True)
     active = models.BooleanField('Active?', default=True)
     
 
 #About Me
-
+class AboutMeModel(Base):
+    desc = models.CharField('Description', max_length=255)
 #Knowledge
 
 #Idioms
