df78c1bd2457c59a18351cda23e42a14c5fe6461
Giorgio Alefe <giorgioa2002@gmail.com>
2022-05-18 16:28:22 -0300

    Create a register in admin to register AboutMeModel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import AboutMeModel
 
 # Register your models here.
+@admin.register(AboutMeModel)
+
+class AboutMeAdmin(admin.ModelAdmin):
+    list_display = ('created', 'modified', 'active')
