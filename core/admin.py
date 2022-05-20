0a107dc35761e007c47e506cf10bfd1ccc496db7
Giorgio Alefe <giorgioa2002@gmail.com>
2022-05-20 18:49:42 -0300

    Register SkillsModel in admin

--- core/admin.py
+++ core/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from .models import AboutMeModel, IdiomsModel
+from .models import AboutMeModel, IdiomsModel, SkillsModel
 
 # Register your models here.
 @admin.register(AboutMeModel)
-@admin.register(IdiomsModel)
-
 class AboutMeAdmin(admin.ModelAdmin):
     list_display = ('created', 'modified', 'active')
 
+@admin.register(IdiomsModel)
 class IdiomsModelAdmin(admin.ModelAdmin):
     list_display = ('lang', 'level', 'created', 'modified')
+
+@admin.register(SkillsModel)
+class SkillsModelAdmin(admin.ModelAdmin):
+    list_display = ('skill', 'percentage', 'created', 'modified', 'active')
