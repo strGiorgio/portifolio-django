28fda5bd00775dbd6cb6d72890da6ed725f88b97
Giorgio Alefe <giorgioa2002@gmail.com>
2022-05-21 18:57:51 -0300

    Import IdiomsModel in view and add a context in indexView

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.urls import reverse_lazy
 
-from .models import AboutMeModel
+from .models import AboutMeModel, IdiomsModel
 
 # Create your views here.
 class IndexView(TemplateView):
@@ -13,4 +13,5 @@ class IndexView(TemplateView):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['title'] = 'strGiorgio'
         context['AboutMeModel'] = AboutMeModel.objects.all()
+        context['IdiomsModel'] = IdiomsModel.objects.all()
         return context
