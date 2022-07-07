8b30509f73f23ca838a22210ec1d450072e1dd8a
Giorgio Alefe <giorgioa2002@gmail.com>
2022-07-07 20:26:09 -0300

    Change TamplatView to FormView, and create a def form_valid and form_invalid to verify message sended

--- core/views.py
+++ core/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 from django.urls import reverse_lazy
 
 from .models import AboutMeModel, IdiomsModel, KnowledgesModel, SkillsModel
 from .forms import UsersMessagesForm
 
 # Create your views here.
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'index.html'
+    form_class = UsersMessagesForm
     success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
@@ -25,5 +26,16 @@ class IndexView(TemplateView):
         context['KnowledgesModel'] = KnowledgesModel.objects.order_by('?').all()
         context['KnowledgesModelCount'] = KnowledgesModel.objects.count()
 
-        context['UsersMessagesForm'] = UsersMessagesForm
+        context['form'] = UsersMessagesForm
         return context
+    
+
+    def form_valid(self, form, *args, **kwargs):
+        form.save()
+        print('Message sended!')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+
+    def form_invalid(self, form, *args, **kwargs):
+        print('Message not sended!')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
